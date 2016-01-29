4649e5d478ef407a86b157252a3c7a67a19312ca
Atze de Vries <atze.devries@naturalis.nl>
2016-01-29 10:05:09 +0100

    stuff

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,7 +27,7 @@
 # Examples
 # --------
 #
-# @example
+# @exampleo
 #    class { 'limsoaipmh':
 #      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
 #    }
@@ -37,16 +37,19 @@
 #
 # Author Name <author@domain.com>
 #
-# Copyright
+# Copyrightdf
 # ---------
+#sd
+# Copyright 2016 Your name here, unless odftherwise noted.
 #
-# Copyright 2016 Your name here, unless otherwise noted.
-#
-class limsoaipmh {
+class role_limsoaipmh (
+  $private_key,
+  $certificate,
+  ) {
 
   class { '::java':  }
 
-  class { 'wildfly':
+  class { '::wildfly':
     version          => '8.2.1',
     install_source   => 'http://download.jboss.org/wildfly/8.2.1.Final/wildfly-8.2.1.Final.tar.gz',
     #group            => 'wildfly',
@@ -56,27 +59,57 @@ class limsoaipmh {
     java_xmx         => '1024m',
     java_xms         => '256m',
     java_maxpermsize => '512m',
+    public_bind      => '127.0.0.1'
     mgmt_bind        => '127.0.0.1',
     users_mgmt       => {
       'wildfly' => {
         password => 'wildfly'
         }
       },
-    require => Class['::java']
+    require          => Class['::java']
   }
 
-  # exec {'create nba conf dir':
-  #   command => '/opt/wildfly/bin/jboss-cli.sh -c command="/system-property=nl.naturalis.nda.conf.dir:add(value=/etc/nba)"',
-  #   unless  => '/opt/wildfly/bin/jboss-cli.sh -c command="ls system-property" | /bin/grep nl.naturalis.nda.conf.dir',
-  #   require => Class['wildfly'],
-  # }
-  #
-  # exec {'create nba logger':
-  #   cwd     => '/opt/wildfly/bin',
-  #   command => '/opt/wildfly/bin/jboss-cli.sh -c command="/subsystem=logging/logger=nl.naturalis.nda:add(level=DEBUG)"',
-  #   unless  => '/opt/wildfly/bin/jboss-cli.sh -c command="ls subsystem=logging/logger" | /bin/grep nl.naturalis.nda',
-  #   require => Class['wildfly'],
-  # }
+  exec {'create nba conf dir':
+    command => '/opt/wildfly/bin/jboss-cli.sh -c command="/system-property=nl.naturalis.oaipmh.conf.dir:add(value=/etc/limsoaipmh)"',
+    unless  => '/opt/wildfly/bin/jboss-cli.sh -c command="ls system-property" | /bin/grep nl.naturalis.oaipmh.conf.dir',
+    require => Class['::wildfly'],
+  }
 
+  file {'/etc/limsoaipmh':
+    ensure => directory,
+    before => Class['::wildfly'],
+  }
+
+  exec {'create lims logger':
+    cwd     => '/opt/wildfly/bin',
+    command => '/opt/wildfly/bin/jboss-cli.sh -c command="/subsystem=logging/logger=nl.naturalis.lims2.oaipmh:add(level=DEBUG)"',
+    unless  => '/opt/wildfly/bin/jboss-cli.sh -c command="ls subsystem=logging/logger" | /bin/grep nl.naturalis.lims2.oaipmh',
+    require => Class['::wildfly'],
+  }
+
+
+  file { '/etc/ssl/lims_key.pem' :
+    ensure  => present,
+    content => $private_key,
+    mode    => '0644',
+  }
+
+  file { '/etc/ssl/lims_cert.pem' :
+    ensure  => present,
+    content => $certificate,
+    mode    => '0644',
+  }
+
+  nginx::resource::upstream { 'limsoaipmh_naturalis_nl':
+    members => ['localhost:8080'],
+  }
+
+  nginx::resource::vhost { 'limsoaipmf.naturalis.nl':
+    proxy       => 'http://limsoaipmh_naturalis_nl',
+    ssl         => true,
+    listen_port => 443,
+    ssl_cert    => '/etc/ssl/lims_cert.pem',
+    ssl_key     => '/etc/ssl/lims_key.pem',
+  }
 
 }
