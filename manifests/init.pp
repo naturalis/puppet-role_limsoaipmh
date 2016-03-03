42469eb8f512f2efd533f400e89a757f4a68b61e
Atze de Vries <atze.devries@naturalis.nl>
2016-03-03 10:48:00 +0100

    another typoi

--- manifests/init.pp
+++ manifests/init.pp
@@ -167,7 +167,7 @@ class role_limsoaipmh (
   exec {'make WAR ':
     cwd         => '/opt/nl.naturalis.oaipmh/nl.naturalis.oaipmh.build',
     command     => '/usr/bin/ant clean build',
-    refreshonly => true
+    refreshonly => true,
     subscribe   => Vcsrepo['/opt/nl.naturalis.oaipmh'],
     require     => [Class['wildfly'],
       Package['ant'],
